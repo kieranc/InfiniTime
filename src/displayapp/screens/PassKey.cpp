b9fc1fca23d6cc281545c14f16e35cf80ec91e5a
James A. Jerkins <evergreen@jamesjerkinscomputer.com>
2021-10-30 18:42:57 -0500

    Fix passkey formatting

--- src/displayapp/screens/PassKey.cpp
+++ src/displayapp/screens/PassKey.cpp
@@ -7,7 +7,7 @@ PassKey::PassKey(Pinetime::Applications::DisplayApp* app, uint32_t key) : Screen
   lpasskey = lv_label_create(lv_scr_act(), nullptr);
   lv_obj_set_style_local_text_color(lpasskey, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, lv_color_hex(0xFFFF00));
   lv_obj_set_style_local_text_font(lpasskey, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &jetbrains_mono_42);
-  lv_label_set_text_fmt(lpasskey, "%60u", key);
+  lv_label_set_text_fmt(lpasskey, "%06u", key);
   lv_obj_align(lpasskey, nullptr, LV_ALIGN_CENTER, 0, -20);
 }
 
