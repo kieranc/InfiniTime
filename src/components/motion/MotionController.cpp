813a30c055736b1299e610e1ebb3cca9e57a5351
Finlay Davidson <finlay.davidson@coderclass.nl>
2021-11-12 00:59:03 +0100

    Improve and simplify Raise to Wake algorithm

--- src/components/motion/MotionController.cpp
+++ src/components/motion/MotionController.cpp
@@ -18,26 +18,18 @@ void MotionController::Update(int16_t x, int16_t y, int16_t z, uint32_t nbSteps)
 }
 
 bool MotionController::ShouldWakeUp(bool isSleeping) {
-  if ((x + 335) <= 670 && z < 0) {
-    if (not isSleeping) {
-      if (y <= 0) {
-        return false;
-      } else {
-        lastYForWakeUp = 0;
-        return false;
-      }
-    }
+  bool ret = false;
 
-    if (y >= 0) {
-      lastYForWakeUp = 0;
-      return false;
-    }
-    if (y + 230 < lastYForWakeUp) {
-      lastYForWakeUp = y;
-      return true;
-    }
+  if (x >= -384 && x <= 384 &&
+      z <= 0 &&
+      y <= lastYForWakeUp - 192 &&
+      isSleeping) {
+    ret = true;
   }
-  return false;
+
+  lastYForWakeUp = (y < 0) ? y : 0;
+
+  return ret;
 }
 void MotionController::IsSensorOk(bool isOk) {
   isSensorOk = isOk;
